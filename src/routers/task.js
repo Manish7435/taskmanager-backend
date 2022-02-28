f558fa6ae8d6014dacabf6ca89eda5f6502d9eba
Manish Kumar <manishkumar@Manishs-MacBook-Air.local>
2022-02-28 19:13:57 +0530

    implemantation of kafka

--- src/routers/task.js
+++ src/routers/task.js
@@ -79,21 +79,6 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 
-
-// router.get('/tasks/:id', auth, async (req, res) => {
-//     const _id = req.params.id
-//     try {
-        
-//         const task = await Task.findOne({ _id, owner: req.user._id })
-//         if (!task) {
-//             return res.status(404).send()
-//         }
-//         res.send(task)
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-// })
-
 router.get('/tasks', auth, async (req, res) => {
     try {
         client.get(req.user._id, (err, data) => {
